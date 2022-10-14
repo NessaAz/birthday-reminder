cdf6dbf5b11d737666bf7e561c62e519ec6b58a6
NessaAz <vanessa.azenwa@gmail.com>
2022-10-14 10:02:47 +0300

    initial project setup

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        name: 'Nathan sykes',
+        age: 6,
+        image: 'https://res.cloudinary.com/nessaaz/image/upload/v1665726374/nathan_ob2pax.jpg',
+    },
+    {
+        id: 2,
+        name: 'Cherop Tai',
+        age: 34,
+        image: 'https://res.cloudinary.com/nessaaz/image/upload/v1665726345/cherop_q2pili.jpg',
+    },
+    {
+        id: 3,
+        name: 'Anje Jolie',
+        age: 39,
+        image: 'https://res.cloudinary.com/nessaaz/image/upload/v1665726354/jolie_nfh7rr.jpg',
+    },
+    {
+        id: 4,
+        name: 'Zoe Saldma',
+        age: 22,
+        image: 'https://res.cloudinary.com/nessaaz/image/upload/v1665726346/zoe_caf4rz.jpg',
+    },
+    {
+        id: 5,
+        name: 'Abdul Azah',
+        age: '29',
+        image: 'https://res.cloudinary.com/nessaaz/image/upload/v1665726354/abdul_ecgcbn.jpg',
+    },
+]
